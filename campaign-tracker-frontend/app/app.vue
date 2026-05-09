4deebbd88e84d6e9512f15dfb6892804c2d5b7d1
Bilal Drideb <bilaldrideb123@gmail.com>
2026-05-09 16:30:17 +0200

    feat: instalacion nuxt.

---
+++ campaign-tracker-frontend/app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+    <ClientOnly>
+      <Toaster position="top-right" richColors />
+    </ClientOnly>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Toaster } from 'vue-sonner'
+import 'vue-sonner/style.css'
+</script>
